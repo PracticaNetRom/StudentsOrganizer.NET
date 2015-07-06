32bdf0146fc76db020d89f9c821c21dab47c209e
Tenea Andrei <andrei.tenea93@gmail.com>
2015-07-06 11:51:02 +0300

    07.06.2015

---
+++ Student_Organizer/MODEL.BO/ParticipantsEventBO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.BO
+{
+    class ParticipantsEventBO
+    {
+        public int ID { get; set; }
+        public int ID_S  { get; set; }
+        public int ID_E { get; set; }
+        public DateTime Period  { get; set; }
+        public string Department { get; set; }
+        public string Task { get; set; }
+    }
+}
