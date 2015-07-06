00aa706ec0b942c917b82a2bf09bc8f50b9cc78f
Andrei Tenea <andrei.tenea93@gmail.com>
2015-07-06 08:47:55 +0300

    Conectare baza de date

---
+++ Student_Organizer/Student_Organizer/ListWithStudents.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Student_Organizer
+{
+    public partial class ListWithStudents : Form
+    {
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
+
+        public ListWithStudents()
+        {
+            InitializeComponent();
+            ShowList();
+         
+        }
+
+        public int ShowList()
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.connection);
+            connection.Open();
+            sda = new SqlDataAdapter(@"SELECT Students.ID, Students.FirstName, Students.LastName, Students.PhoneNumber, Students.Faculty, Students.FacultyStartYear FROM Students", connection);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scb = new SqlCommandBuilder(sda);
+            sda.Update(dt);
+            ShowList();
+        }
+
+    }
+}
---
+++ Student_Organizer/Student_Organizer/LogIn.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Student_Organizer
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+            textBox2.PasswordChar = '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.connection);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM LogIn WHERE Username = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "' ", connection);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            int count = 0;
+
+            while(dr.Read())
+            {
+                count += 1;
+            }
+
+            if(count == 1)
+            { 
+                MainForm andrei = new MainForm();
+                andrei.Show();
+                
+                
+            }
+
+            else if(count > 0)
+            {
+                MessageBox.Show("Duplicate username");
+            }
+
+            else
+            {
+                MessageBox.Show("Username or password is incorrect!");
+            }
+
+            textBox1.Clear();
+            textBox2.Clear();
+            this.Hide();
+          
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
