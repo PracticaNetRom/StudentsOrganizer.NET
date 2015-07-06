08f1850890dcab842196943dd44069aa4b308f39
LazarCostinel <costinellazar@gmail.com>
2015-07-07 00:56:46 +0300

    Done

---
+++ Practica/Practica/Studenti.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Practica
+{
+    partial class Evidenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.data2 = new System.Windows.Forms.DateTimePicker();
+            this.data1 = new System.Windows.Forms.DateTimePicker();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.idText = new System.Windows.Forms.TextBox();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.LastText = new System.Windows.Forms.TextBox();
+            this.genderText = new System.Windows.Forms.TextBox();
+            this.facText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(100, 257);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Insert";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(400, 257);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Evenimente";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(44, 51);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(55, 13);
+            this.labelControl1.TabIndex = 2;
+            this.labelControl1.Text = "First Name:";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(44, 79);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(54, 13);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "Last Name:";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(44, 109);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(35, 13);
+            this.labelControl3.TabIndex = 8;
+            this.labelControl3.Text = "Gender";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(44, 139);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(52, 13);
+            this.labelControl4.TabIndex = 6;
+            this.labelControl4.Text = "Birth Date:";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(44, 168);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(39, 13);
+            this.labelControl5.TabIndex = 14;
+            this.labelControl5.Text = "Faculty:";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(44, 194);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(66, 13);
+            this.labelControl6.TabIndex = 12;
+            this.labelControl6.Text = "Faculty Start:";
+            // 
+            // data2
+            // 
+            this.data2.Location = new System.Drawing.Point(140, 194);
+            this.data2.Name = "data2";
+            this.data2.Size = new System.Drawing.Size(184, 20);
+            this.data2.TabIndex = 16;
+            // 
+            // data1
+            // 
+            this.data1.Location = new System.Drawing.Point(140, 133);
+            this.data1.Name = "data1";
+            this.data1.Size = new System.Drawing.Size(184, 20);
+            this.data1.TabIndex = 17;
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(44, 22);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(15, 13);
+            this.labelControl7.TabIndex = 18;
+            this.labelControl7.Text = "ID:";
+            // 
+            // idText
+            // 
+            this.idText.Location = new System.Drawing.Point(140, 19);
+            this.idText.Name = "idText";
+            this.idText.Size = new System.Drawing.Size(100, 20);
+            this.idText.TabIndex = 19;
+            // 
+            // nameText
+            // 
+            this.nameText.Location = new System.Drawing.Point(140, 48);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(100, 20);
+            this.nameText.TabIndex = 20;
+            // 
+            // LastText
+            // 
+            this.LastText.Location = new System.Drawing.Point(140, 79);
+            this.LastText.Name = "LastText";
+            this.LastText.Size = new System.Drawing.Size(100, 20);
+            this.LastText.TabIndex = 21;
+            // 
+            // genderText
+            // 
+            this.genderText.Location = new System.Drawing.Point(140, 106);
+            this.genderText.Name = "genderText";
+            this.genderText.Size = new System.Drawing.Size(100, 20);
+            this.genderText.TabIndex = 22;
+            // 
+            // facText
+            // 
+            this.facText.Location = new System.Drawing.Point(140, 165);
+            this.facText.Name = "facText";
+            this.facText.Size = new System.Drawing.Size(100, 20);
+            this.facText.TabIndex = 23;
+            // 
+            // Evidenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 334);
+            this.Controls.Add(this.facText);
+            this.Controls.Add(this.genderText);
+            this.Controls.Add(this.LastText);
+            this.Controls.Add(this.nameText);
+            this.Controls.Add(this.idText);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.data1);
+            this.Controls.Add(this.data2);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Evidenta";
+            this.Text = "Evidenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private System.Windows.Forms.DateTimePicker data2;
+        private System.Windows.Forms.DateTimePicker data1;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private System.Windows.Forms.TextBox idText;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.TextBox LastText;
+        private System.Windows.Forms.TextBox genderText;
+        private System.Windows.Forms.TextBox facText;
+    }
+}
+
---
+++ Practica/Practica/Studenti.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace Practica
+{
+    public partial class Evidenta : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=SNAP;Initial Catalog=Evidenta;Integrated Security=True");
+        SqlCommand cmd;
+
+        public Evidenta()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("INSERT INTO [dbo].[Evidenta] ([ID], [FirstName], [LastName], [Gender], [BirthDate], [Faculty], [FacultyStartYear]) values('" + idText.Text + "','" + nameText.Text + "','" + LastText.Text + "','" + genderText.Text + "','" + data1.Value.ToString("dd/MM/yyyy") + "','" + facText.Text + "','" + data2.Value.ToString("dd/MM/yyyy") + "')", con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Update Successfull to the Database");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            Events ev = new Events();
+            ev.Show();
+        }
+    }
+}
