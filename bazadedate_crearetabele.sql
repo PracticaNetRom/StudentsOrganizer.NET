66573ec70ce6488b2ae194bc9aec2010da7e9ada
Marina Amalia <marinescumra@yahoo.com>
2015-07-08 11:52:15 +0300

    baza
    baza de date

--- bazadedate_crearetabele.sql
+++ bazadedate_crearetabele.sql
@@ -1,41 +1,48 @@
+
+
 create table student (
-	idstudent NUMBER NOT NULL,
-	Ftname VARCHAR(15),
+	id_student int NOT NULL,
+	Fname VARCHAR(15),
 	Lname VARCHAR(15),
-	Gender NUMBER,
+	Gender int,
 	Birthdate DATE,
 	Email VARCHAR(10),
 	PhoneNumbers VARCHAR(22),
 	Faculty VARCHAR(50),
 	FacultyStartYear VARCHAR(30),
-	CONSTRAINT participants_id_pk PRIMARY KEY(idparticipant)
+	CONSTRAINT participants_id_pk PRIMARY KEY(id_student)
 );
 
 create table event(
-	id_event NUMBER NOT NULL,
+	id_event int NOT NULL,
 	Period VARCHAR(20),
 	Department VARCHAR(50),
 	Tasks VARCHAR(100),
 	Remarks VARCHAR(200),
-	Event
+	Event_Type_id int,
 	CONSTRAINT event_id_pk PRIMARY KEY(id_event)
 );
 
-create table students_event(
-	id NUMBER NOT NULL,
-	idstudent NUMBER ,
-	idevent NUMBER,
-	period DATE,
-	department VARCHAR(100),
-	task VARCHAR(100),
-	remarks VARCHAR(200)
-	CONSTRAINT students_event_id_pk PRIMARY KEY(id)	
+create table student_event(
+	id_student int  NOT NULL ,
+	id_event int NOT NULL ,
+	CONSTRAINT Studentevent_id_pk PRIMARY KEY(id_student, id_event)
 );
 
-ALTER TABLE students_event 
-ADD(CONSTRAINT students_event_fk FOREIGN KEY(idstudent) 
-REFERENCES students(idstudent));
-ADD(CONSTRAINT participants_event_fk FOREIGN KEY(idevent) 
-REFERENCES event(idevent));
+create table EventTypes(
+	Event_Type_id int NOT NULL PRIMARY KEY,
+	Description VARCHAR(200)
+);
+
+ALTER TABLE student_event 
+ADD CONSTRAINT student_event_fk FOREIGN KEY (id_student) REFERENCES student(id_student)
+ALTER TABLE student_event
+ADD CONSTRAINT student_event_fk2 FOREIGN KEY(id_event) REFERENCES event(id_event)
+
+ALTER TABLE event
+ADD FOREIGN KEY (Event_Type_id) 
+REFERENCES EventTypes(Event_Type_id)
+
+
 
 
