c95e17392041f445c8d7fb1be53c6ee873f566e8
Petre Mazilu <mazilu_petre@yahoo.ro>
2015-07-13 11:59:38 +0300

    UserControlExample
    UserControlExample

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            userControl11.ButtonClicked += userControl11_ButtonClicked;
+        }
+
+        void userControl11_ButtonClicked()
+        {
+            MessageBox.Show("User Control button clicked");
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/UserControl1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class UserControl1 : UserControl
+    {
+        public delegate void ClickHandler();
+        public event ClickHandler ButtonClicked;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ButtonClicked != null)
+            {
+                ButtonClicked();
+            }
+        }
+    }
+}
